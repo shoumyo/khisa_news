00984f73c8e2a7ab5d64b2fce6ae42db7184108f
Shoumyo <shoumyosrijito@gmail.com>
2026-06-27 11:12:18 +0600

    fix: correct image width class in Header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/logo.png';
 const Header = () => {
     return (
         <div className=' flex justify-center flex-col items-center gap-3 m-4'>
-            <img className='w-87.5' src={logo} alt="logo" />
+            <img className='w-[400]' src={logo} alt="logo" />
             <p className='text-accent'>Journalism Without Fear or Favour</p>
         </div>
     );
