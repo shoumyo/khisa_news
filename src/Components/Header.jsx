8f514a592c63978751cf60b55975e3b352150b18
Shoumyo <shoumyosrijito@gmail.com>
2026-06-27 11:22:38 +0600

    feat: add date-fns dependency and display formatted date in Header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from '../assets/logo.png';
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className=' flex justify-center flex-col items-center gap-3 m-4'>
             <img className='w-[400]' src={logo} alt="logo" />
             <p className='text-accent'>Journalism Without Fear or Favour</p>
+            <p className='text-semibold text-accent'>{format(new Date(),"EEEE , MMMM MM , yyyy")}</p>
         </div>
     );
 };
