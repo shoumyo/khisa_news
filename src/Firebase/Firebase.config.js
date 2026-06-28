3522492c800870423e66865caa9158a8117583dc
Shoumyo <shoumyosrijito@gmail.com>
2026-06-28 11:37:29 +0600

    feat: integrate Firebase and add authentication context
    - Added Firebase SDK to package.json for authentication functionality.
    - Created Firebase configuration file to initialize Firebase app.
    - Implemented AuthProvider to manage user authentication state using React Context API.
    - Updated NavBar component to display user email and conditionally render user image.
    - Wrapped RouterProvider with AuthProvider in main entry point for context availability.

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBqFzLTDg72w5iBrHHJ3V-yiPPrxRbwcGA',
+  authDomain: 'khisa-news.firebaseapp.com',
+  projectId: 'khisa-news',
+  storageBucket: 'khisa-news.firebasestorage.app',
+  messagingSenderId: '14465787737',
+  appId: '1:14465787737:web:2c75442e57181967334e48',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,14 @@
+import React, { createContext, useState } from 'react';
+export const AuthContext=createContext();
+const AuthProvider = ({children}) => {
+    const[user,setUser]=useState([]);
+    const authData={
+        user,
+        setUser,
+    }
+    return (
+        <AuthContext value={authData}>{children}</AuthContext>
+    );
+};
+
+export default AuthProvider;
