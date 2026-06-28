fafdfbbe63c8f7f30b0e3f1f84035013824d87de
Shoumyo <shoumyosrijito@gmail.com>
2026-06-28 10:35:25 +0600

    feat: restructure Category and Home components, update import paths, and implement news filtering logic

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,38 +1,36 @@
-import { createBrowserRouter } from "react-router";
-import HomeLayout from "../Layouts/HomeLayout";
-import Home from "../Components/Pages/Home";
-import Category from "../Components/Pages/Category";
+import { createBrowserRouter } from 'react-router';
+import HomeLayout from '../Layouts/HomeLayout';
+import Home from '../Pages/Home';
+import Category from '../Pages/Category';
 
-const router=createBrowserRouter(
-    [
-        {
-            path:'/',
-            Component:HomeLayout,
-            children:[
-                {
-                    path:"",
-                    Component:Home,
-                },
-                {
-                    path:"/category/:id",
-                    Component:Category,
-                    loader:()=>fetch("/news.json"),
-                }
-            ]
-        },
-        {
-            path:'/auth',
-            element:<h2>Auth Layout</h2>
-        },
-        {
-            path:'/news',
-            element:<h2>News Layout</h2>
-        },
-        {
-            path:'/*',
-            element:<h2>eRROR404</h2>
-        }
-    ]
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: HomeLayout,
+    children: [
+      {
+        path: '',
+        Component: Home,
+      },
+      {
+        path: '/category/:id',
+        Component: Category,
+        loader: () => fetch('/news.json'),
+      },
+    ],
+  },
+  {
+    path: '/auth',
+    element: <h2>Auth Layout</h2>,
+  },
+  {
+    path: '/news',
+    element: <h2>News Layout</h2>,
+  },
+  {
+    path: '/*',
+    element: <h2>eRROR404</h2>,
+  },
+]);
 
 export default router;
